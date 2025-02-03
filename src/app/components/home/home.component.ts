f8ed7805495e12e1e50ffe093ff8bf8b83c3b3f2
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-03 19:36:31 +0530

    Added Dashbaord Grid to home Page with Images for grid

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import {MatGridListModule} from '@angular/material/grid-list';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

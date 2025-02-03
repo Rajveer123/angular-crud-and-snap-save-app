cea89f906104e0e2a76f6cbc1f638efbd71d0249
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-03 21:54:18 +0530

    Implemented custom directive to provide hover effect to Home page grid tile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink} from '@angular/router';
+import { HoverClassDirective } from '../../custom-directives/hover-class.directive';
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, RouterLink, RouterLinkActive],
+  imports: [MatGridListModule, RouterLink, HoverClassDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

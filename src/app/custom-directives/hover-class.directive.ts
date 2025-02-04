852f9807176fe7f5198c92f3dbc781b877f2f9eb
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-04 08:49:05 +0530

    Added the Employe Component and starting Form UI.

--- src/app/custom-directives/hover-class.directive.ts
+++ src/app/custom-directives/hover-class.directive.ts
@@ -1,23 +1,22 @@
 import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
-  selector: '[hover-class]' //appHoverClass
+  selector: '[hover-class]'
 })
 export class HoverClassDirective {
 
   constructor(public elementRef: ElementRef) { }
   @Input('hover-class') hoverClass: any;
 
-    @HostListener('mouseenter') onMouseEnter() {
-        this.elementRef.nativeElement.style.border = '2px solid black';
-        this.elementRef.nativeElement.style.backgroundColor = 'cornflowerblue';
-        this.elementRef.nativeElement.style.borderRadius = '10px';
-    }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.elementRef.nativeElement.style.border = '2px solid black';
+    this.elementRef.nativeElement.style.backgroundColor = 'cornflowerblue';
+    this.elementRef.nativeElement.style.borderRadius = '10px';
+  }
 
-    @HostListener('mouseleave') onMouseLeave() {
-         this.elementRef.nativeElement.style.border = '2px solid transparent';
-         this.elementRef.nativeElement.style.backgroundColor = 'transparent';
-         this.elementRef.nativeElement.style.borderRadius = '0px';
-    }
-
+  @HostListener('mouseleave') onMouseLeave() {
+    this.elementRef.nativeElement.style.border = '2px solid transparent';
+    this.elementRef.nativeElement.style.backgroundColor = 'transparent';
+    this.elementRef.nativeElement.style.borderRadius = '0px';
+  }
 }

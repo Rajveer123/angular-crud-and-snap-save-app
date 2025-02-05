a4c44ecb4e80677d90a70c2d983df3f57a880128
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-05 11:58:11 +0530

    Display Data on Employee Lsit Page

--- src/app/components/employ-list/employ-list.component.ts
+++ src/app/components/employ-list/employ-list.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { Employee } from '../../models/employ.model';
+import { Router } from '@angular/router';
+import { EmployService } from '../../sevices/employ.service';
 
+const EMPLOYEE_LIST: Employee[] = [
+  {
+    employeeId: 1,
+    employeeName: 'John Doe',
+    employeeEmailAdress: 'johndoe@example.com',
+    employeeGender: 'Male',
+    employeeContactNumber: '+1 123-456-7890',
+    employeeDepartment: 'IT',
+    employeeSkills: 'Java, C#,.NET'
+  }
+];
 @Component({
   selector: 'app-employ-list',
-  imports: [],
   templateUrl: './employ-list.component.html',
-  styleUrl: './employ-list.component.css'
+  styleUrl: './employ-list.component.css',
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule],
 })
 export class EmployListComponent {
-
+  displayedColumns: string[] = ['employeeId', 'employeeName', 'employeeEmailAdress', 'employeeDepartment','employeeSkills'];
+  dataSource = new MatTableDataSource(EMPLOYEE_LIST);
+  // constructor(private route: Router, private services: EmployService) {
+  // }
+  // ngOnInit(): void {
+  //   this.EMPLOYEE_LIST = this.services.getEmployesses();
+  //   console.log('Employees: ', this.EMPLOYEE_LIST);
+  // } 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  
 }

e633d242aae7345a1230a69dc54e82c763f137ba
Rajveer Singh <rajveersingh.charan@gmail.com>
2025-02-05 10:29:20 +0530

    Setup EmployList component on Dashbaord to show List of Employes

---
+++ src/app/components/employ-list/employ-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employ-list',
+  imports: [],
+  templateUrl: './employ-list.component.html',
+  styleUrl: './employ-list.component.css'
+})
+export class EmployListComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { RouterLink} from '@angular/router';
 import { HoverClassDirective } from '../../custom-directives/hover-class.directive';
+import { EmployListComponent } from '../employ-list/employ-list.component';
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, RouterLink, HoverClassDirective],
+  imports: [MatGridListModule, RouterLink, HoverClassDirective, EmployListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
